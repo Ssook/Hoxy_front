198808a3163691645dd1ab85a18ff4c884f5d15d
kyBang <avaunt13@naver.com>
2020-04-09 20:07:00 +0900

    main3
    갤러리 사진 불러오기 만들예정!!

--- hoxy/app/src/main/java/org/techtown/hoxy/ui/home/HomeFragment.java
+++ hoxy/app/src/main/java/org/techtown/hoxy/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package org.techtown.hoxy.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,19 +17,73 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    public void community_click(){   //커뮤니티 클릭시 커뮤니티 페이지로 인텐
+        //  Intent a = new Intent(getApplicationContext(),SubActivity.class);
+        // startActivity( a);
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView hometextView = root.findViewById(R.id.text_home); //혹시 어떻게 버리지?
-       // final TextView textView2 = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final TextView home_textView = root.findViewById(R.id.text_home); //혹시 어떻게 버리지??
+        final TextView content_textView = root.findViewById(R.id.text_content1); // 첫번째 게시글
+        final TextView content2_textView = root.findViewById(R.id.text_content2); // 두번째 게시글
+        final TextView content3_textView = root.findViewById(R.id.text_content3); // 세번째 게시글
+        final TextView content1_time_textView = root.findViewById(R.id.text_content1_time); // 첫번째 게시글 작성일
+        final TextView content2_time_textView = root.findViewById(R.id.text_content2_time); // 첫번째 게시글 작성일
+        final TextView content3_time_textView = root.findViewById(R.id.text_content3_time); // 첫번째 게시글 작성일
+
+
+        homeViewModel.getTitle().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                hometextView.setText(s);
+                home_textView.setText(s);
             }
         });
-        return root;
+
+        homeViewModel.getContent1().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                content_textView.setText(s);
+            }
+        });
+
+        homeViewModel.getContent2().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                content2_textView.setText(s);
+            }
+        });
+
+        homeViewModel.getContent3().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                content3_textView.setText(s);
+            }
+        });
+
+        homeViewModel.getContent1Time().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                content1_time_textView.setText(s);
+            }
+        });
+
+        homeViewModel.getContent2Time().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                content2_time_textView.setText(s);
+            }
+        });
+
+        homeViewModel.getContent3Time().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                content3_time_textView.setText(s);
+            }
+        });
+
+    return root;
     }
 }
