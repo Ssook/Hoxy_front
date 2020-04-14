4ca27a918d76f59187b9985eccb3a74acd9206a0
kss <tjdtn9692@gmail.com>
2020-04-14 17:54:44 +0900

    comment section

--- app/src/main/java/org/techtown/hoxy/FragmentCallback.java
+++ app/src/main/java/org/techtown/hoxy/FragmentCallback.java
@@ -3,5 +3,5 @@ package org.techtown.hoxy;
 import org.w3c.dom.Comment;
 
 public interface FragmentCallback {
-    public void onCommand(String command );
+   public void onCommand(String command);
 }
