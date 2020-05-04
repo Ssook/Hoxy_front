d30ad822e5566a84099d87dd0d8e41d57f76f114
kyBang <avaunt13@naver.com>
2020-05-04 18:20:04 +0900

    수정
    HomeViewModel 삭제
    result 페이지 수정

--- app/src/main/java/org/techtown/hoxy/ui/home/HomeFragment.java
+++ app/src/main/java/org/techtown/hoxy/ui/home/HomeFragment.java
@@ -21,13 +21,7 @@ import org.techtown.hoxy.ui.image.ImageFragment;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
-    public void community_click(){   //커뮤니티 클릭시 커뮤니티 페이지로 인텐
-        //  Intent a = new Intent(getApplicationContext(),SubActivity.class);
-        // startActivity( a);
-    }
-
+    String text_title, text_content1, text_content2, text_content3, text_content1_time, text_content2_time, text_content3_time;
         public static HomeFragment newinstance(){
             return new HomeFragment();
         }
@@ -35,9 +29,8 @@ public class HomeFragment extends Fragment {
 @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView home_textView = root.findViewById(R.id.text_home); //혹시 어떻게 버리지??
+        final TextView home_main_textView = root.findViewById(R.id.text_home); //혹시 어떻게 버리지??
         final TextView content_textView = root.findViewById(R.id.text_content1); // 첫번째 게시글
         final TextView content2_textView = root.findViewById(R.id.text_content2); // 두번째 게시글
         final TextView content3_textView = root.findViewById(R.id.text_content3); // 세번째 게시글
@@ -45,55 +38,25 @@ public class HomeFragment extends Fragment {
         final TextView content2_time_textView = root.findViewById(R.id.text_content2_time); // 첫번째 게시글 작성일
         final TextView content3_time_textView = root.findViewById(R.id.text_content3_time); // 첫번째 게시글 작성일
 
+        text_title = "혹시 어떻게 버리지..?";
 
-        homeViewModel.getTitle().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                home_textView.setText(s);
-            }
-        });
+        text_content1 = "1번 게시글";
+        text_content2 = "2번 게시글";
+        text_content3 = "3번 게시글";
 
-        homeViewModel.getContent1().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                content_textView.setText(s);
-            }
-        });
+        text_content1_time = "2020-04-08";
+        text_content2_time = "2020-04-09";
+        text_content3_time = "2020-04-10";
 
-        homeViewModel.getContent2().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                content2_textView.setText(s);
-            }
-        });
 
-        homeViewModel.getContent3().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                content3_textView.setText(s);
-            }
-        });
+                home_main_textView.setText(text_title);
+                content_textView.setText(text_content1);
+                content2_textView.setText(text_content2);
+                content3_textView.setText(text_content3);
+                content1_time_textView.setText(text_content1_time);
+                content2_time_textView.setText(text_content2_time);
+                content3_time_textView.setText(text_content3_time);
 
-        homeViewModel.getContent1Time().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                content1_time_textView.setText(s);
-            }
-        });
-
-        homeViewModel.getContent2Time().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                content2_time_textView.setText(s);
-            }
-        });
-
-        homeViewModel.getContent3Time().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                content3_time_textView.setText(s);
-            }
-        });
 
 
     ////////////////갤러리로 넘어가기
@@ -118,5 +81,7 @@ public class HomeFragment extends Fragment {
     });
     return root;
     }
+
+
 
 }
