861864d3203d7c5e14eda38a2a46fad689a8a982
kyBang <avaunt13@naver.com>
2020-05-04 21:47:02 +0900

    수정했슴둥

--- app/src/main/java/org/techtown/hoxy/ui/image/ImageFragment.java
+++ app/src/main/java/org/techtown/hoxy/ui/image/ImageFragment.java
@@ -1,34 +1,20 @@
 package org.techtown.hoxy.ui.image;
 
-import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Context;
-import android.content.ContentResolver;
-
-
-
 import androidx.fragment.app.Fragment;
-
 import com.bumptech.glide.Glide;
-
 import org.techtown.hoxy.MainActivity;
 import org.techtown.hoxy.R;
 import org.techtown.hoxy.ui.result.ResultFragment;
-
 import java.io.InputStream;
 
 import static android.app.Activity.RESULT_OK;
@@ -41,14 +27,18 @@ public class ImageFragment extends Fragment {
 
 
 
-
     public static  ImageFragment newinstance(){
         return new ImageFragment();
     }
 
     private static final int REQUEST_CODE = 0;
-    ImageView imageView;
-    String trash = "소파입니다";
+    private ImageView trash_ImageView;
+    private TextView trash_textView;
+    private Button again_button, next_button;
+
+
+    private Bitmap trash_bitmap;
+    private String trashName ;
 
 
 
@@ -57,28 +47,23 @@ public class ImageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        ///////////////////////////////////////
-        Fragment fragment = new ResultFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString("Trash", trash);
-        fragment.setArguments(bundle);
-        /////////////////////////////////////////////
+
         View root = inflater.inflate(R.layout.fragment_image, container, false);
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, REQUEST_CODE);
 
-        TextView textView = root.findViewById(R.id.textView);
-        textView.setText(trash);
+
+        trash_textView = root.findViewById(R.id.textView);
 
 
-       imageView = (ImageView) root.findViewById(R.id.imageView);
+        trash_ImageView = (ImageView) root.findViewById(R.id.imageView);
 
-        Button button = root.findViewById(R.id.button);
-        Button button2 = root.findViewById(R.id.button2);
+        again_button = root.findViewById(R.id.button);
+        next_button = root.findViewById(R.id.button2);
     //// 다시
-        button.setOnClickListener(new View.OnClickListener(){
+        again_button.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
@@ -87,7 +72,7 @@ public class ImageFragment extends Fragment {
             }
         });
     //// 다음
-        button2.setOnClickListener(new View.OnClickListener(){
+        next_button.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
@@ -114,10 +99,13 @@ public class ImageFragment extends Fragment {
                     System.out.println("2");
                     // 선택한 이미지에서 비트맵 생성
                     InputStream in =  ((MainActivity)getActivity()).getContentResolver().openInputStream(data.getData());
-                    Bitmap img = BitmapFactory.decodeStream(in);
+                    trash_bitmap = BitmapFactory.decodeStream(in);
                     in.close();
+                    TrashName.setTrash("쇼파일껄?");
+                    trashName = TrashName.getTrash();
+                    trash_textView.setText(trashName);
                     // 이미지 표시
-                    Glide.with(this).load(img).into(imageView);
+                    Glide.with(this).load(trash_bitmap).into(trash_ImageView);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
--- app/src/main/java/org/techtown/hoxy/ui/result/ResultFragment.java
+++ app/src/main/java/org/techtown/hoxy/ui/result/ResultFragment.java
@@ -5,19 +5,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import org.techtown.hoxy.MainActivity;
 import org.techtown.hoxy.R;
+import org.techtown.hoxy.ui.home.HomeFragment;
+import org.techtown.hoxy.ui.image.ImageFragment;
+import org.techtown.hoxy.ui.image.TrashName;
 
 public class ResultFragment extends Fragment {
 
-    String trash = "trash";
-    final String[] Example = new String[]{"Test1" ,"Test2" ,"Test3" ,"Test4"};
-
+    private String trashName = TrashName.getTrash();
+    private String[] trashSizeArray = new String[]{"Test1" ,"Test2" ,"Test3" ,"Test4"};
+    private Button next_button, again_button, howto_button;
+    private TextView trash_code_textView, trash_fee_textView;
+    private String trash_code, trash_fee;
     public static ResultFragment newinstance() {
         return new ResultFragment();
     }
@@ -26,29 +32,58 @@ public class ResultFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        if(getArguments() != null) {
-            System.out.println("00");
 
-            Bundle bundle = getArguments();
-            trash = bundle.getString("Trash");
-        }
         final View root = inflater.inflate(R.layout.fragment_result, container, false);
         Spinner spinner = (Spinner)root.findViewById(R.id.spinner);
 
-        final TextView textView = root.findViewById(R.id.textView2);
-        final TextView textView1 = root.findViewById(R.id.textView4);
+        trash_code_textView = root.findViewById(R.id.textView2);
+        trash_fee_textView  = root.findViewById(R.id.textView4);
+        again_button = root.findViewById(R.id.button3);
+        howto_button = root.findViewById(R.id.button6);
+        next_button = root.findViewById(R.id.button5);
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                textView1.setText(parent.getItemAtPosition(position).toString());
+                //textView1.setText(parent.getItemAtPosition(position).toString());
 
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                textView.setText(trash);
+                //textView.setText(trashName);
             }
         });
+
+
+        //// 다시
+        again_button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                ((MainActivity)getActivity()).replaceFragment(HomeFragment.newinstance());
+            }
+        });
+        //// 다음
+        next_button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                ((MainActivity)getActivity()).replaceFragment(ResultFragment.newinstance());
+            }
+        });
+
+        howto_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
         return root;
     }
 }
