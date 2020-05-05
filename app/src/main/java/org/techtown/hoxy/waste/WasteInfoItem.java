a8f1536ae1132b41978836b7f8a6fe45b4b312bd
kyBang <avaunt13@naver.com>
2020-05-05 18:27:39 +0900

    wasteItemInfo

---
+++ app/src/main/java/org/techtown/hoxy/waste/WasteInfoItem.java
@@ -0,0 +1,47 @@
+package org.techtown.hoxy.waste;
+
+import java.util.ArrayList;
+
+public class WasteInfoItem {
+
+    private String waste_name;
+    private ArrayList<String> waste_size;
+    private int waste_fee;
+    private int waste_No;
+    private String img_Url;
+
+   /* public String getWaste_name() {
+        return waste_name;
+    }
+
+    public ArrayList<String> getWaste_size() {
+        return waste_size;
+    }
+
+    public int getWaste_fee() {
+        return waste_fee;
+    }
+
+    public int getWaste_No() {
+        return waste_No;
+    }
+
+    public void setWaste_name(String waste_name) {
+        this.waste_name = waste_name;
+    }
+
+    public void setWaste_size(ArrayList<String> waste_size) {
+        this.waste_size = waste_size;
+    }
+
+    public void setWaste_fee(int waste_fee) {
+        this.waste_fee = waste_fee;
+    }
+
+    public void setWaste_No(int waste_No) {
+        this.waste_No = waste_No;
+    }*/
+
+
+
+}
