873499f5d2cd04cfee9e329dff51e53ee815018e
jamwomsoo <41579282+jamwomsoo@users.noreply.github.com>
2020-06-04 19:41:28 +0900

    mypageview 수정

--- app/src/main/java/org/techtown/hoxy/NoticeItem.java
+++ app/src/main/java/org/techtown/hoxy/NoticeItem.java
@@ -6,8 +6,19 @@ import android.graphics.Bitmap;
 public class NoticeItem {
         String notice;
 
-        public NoticeItem() {
+        public NoticeItem(String notice) {
+                this.notice = notice;
+        }
+        public NoticeItem(){
+                this.notice = "";
+        }
 
+        public String getNotice() {
+                return notice;
+        }
+
+        public void setNotice(String notice) {
+                this.notice = notice;
         }
 }
 
